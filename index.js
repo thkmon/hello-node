904b9f62b4ca52400c9f7824f82cfec7d8d559c3
thkmon <thkmon@gmail.com>
2021-02-04 02:14:36 +0900

    express study

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const uuid4 = require('uuid4');
-console.log(uuid4());
+const http = require('http');
+
+http.createServer((request, response)=>{
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
+    response.write('Hello World');
+    response.end();
+}).listen(3000);
